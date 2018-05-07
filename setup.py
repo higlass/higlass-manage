b5cd61739a28fc25e9bf4c2ef26083ee39caeeb0
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2018-05-07 14:50:29 -0400

    New version of clodius in the requirements

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     install_requires=[
         'Click',
         'hgtiles==v0.2.1',
-        'clodius==v0.8.0'
+        'clodius==v0.9.0'
     ],
     entry_points='''
         [console_scripts]
