1320aa72b498c9a3cf35bd084a5fcc1872ff14da
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2018-05-07 14:44:32 -0400

    Added initial bumpversion

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='higlass-manage',
-    version='0.1',
+    version='0.1.0',
     py_modules=['higlass_manage'],
     install_requires=[
         'Click',
