9035d0616a5501018ce4ec8623702046409709bc
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2018-05-07 14:44:36 -0400

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='higlass-manage',
-    version='0.1.0',
+    version='0.1.1',
     py_modules=['higlass_manage'],
     install_requires=[
         'Click',
