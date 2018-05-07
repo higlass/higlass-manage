59190dfd30e06ee9f9f6a976b29e7dc1a4acd626
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2018-05-07 14:58:41 -0400

    Updated setup metadata and dependencies

--- setup.py
+++ setup.py
@@ -2,12 +2,17 @@ from setuptools import setup
 
 setup(
     name='higlass-manage',
+    author='Peter Kerpedjiev',
+    author_email='pkerpedjiev@gmail.com',
+    url='https://github.com/pkerpedjiev/higlass-manage',
+    description='Wrappers for running the HiGlass Docker container',
     version='0.1.2',
     py_modules=['higlass_manage'],
     install_requires=[
         'Click',
         'hgtiles==v0.2.1',
-        'clodius==v0.9.0'
+        'clodius==v0.9.0',
+        'docker'
     ],
     entry_points='''
         [console_scripts]
