8b136774a51868788f3d580043b3402f139def8d
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-10-17 13:03:16 -0400

    Add new failing subprocess call, and check all subprocess calls

--- test.py
+++ test.py
@@ -7,19 +7,21 @@ import time
 sp.call('bash get_test_data.sh', shell=True)
 
 if op.exists('test-hg_data'):
-    sp.call('rm -rf test-hg-data', shell=True)
+    sp.check_call('rm -rf test-hg-data', shell=True)
 if op.exists('test-hg-media'):
-    sp.call('rm -rf test-hg-media', shell=True)
+    sp.check_call('rm -rf test-hg-media', shell=True)
 
 # directories that will store data and media
-sp.call('mkdir test-hg-data', shell=True)
-sp.call('mkdir test-hg-media', shell=True)
+sp.check_call('mkdir test-hg-data', shell=True)
+sp.check_call('mkdir test-hg-media', shell=True)
 
-sp.call('cp data/Dixon2012-J1-NcoI-R1-filtered.100kb.multires.cool test-hg-media/dixon.mcool', 
+sp.check_call('cp data/Dixon2012-J1-NcoI-R1-filtered.100kb.multires.cool test-hg-media/dixon.mcool',
         shell=True)
 
-sp.call('python higlass_manage.py start --port 8123 --hg-name test-hg --data-dir $(pwd)/test-hg-data --media-dir $(pwd)/test-hg-media', shell=True)
-sp.call('python higlass_manage.py ingest --hg-name test-hg --no-upload /media/dixon.mcool --uid a', shell=True)
+sp.check_call('python higlass_manage.py view test-hg-media/dixon.mcool', shell=True)
+
+sp.check_call('python higlass_manage.py start --port 8123 --hg-name test-hg --data-dir $(pwd)/test-hg-data --media-dir $(pwd)/test-hg-media', shell=True)
+sp.check_call('python higlass_manage.py ingest --hg-name test-hg --no-upload /media/dixon.mcool --uid a', shell=True)
 
 # first one will return bad gateway
 out = sp.check_output('curl localhost:8123/api/v1/tilesets/', shell=True).decode('utf8')
@@ -31,9 +33,9 @@ while out.find('Bad Gateway') >= 0:
 print("out:", out)
 # make sure our file was successfully added
 assert(out.find('dixon.mcool') >= 0)
-sp.call('python higlass_manage.py stop test-hg', shell=True)
+sp.check_call('python higlass_manage.py stop test-hg', shell=True)
 
-sp.call('rm -rf test-hg-data', shell=True)
-sp.call('rm -rf test-hg-media', shell=True)
+sp.check_call('rm -rf test-hg-data', shell=True)
+sp.check_call('rm -rf test-hg-media', shell=True)
 
 print("Passed all tests")
