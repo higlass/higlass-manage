fcb41aaffee3917d5f56e13b986a48020d0a9ea5
Nezar Abdennur <nabdennur@gmail.com>
2019-12-05 10:45:23 -0500

    Turn version subcommand into a top-level option

--- higlass_manage/cli.py
+++ higlass_manage/cli.py
@@ -1,5 +1,6 @@
 import click
 
+from higlass_manage import __version__
 from higlass_manage.ingest import ingest
 from higlass_manage.list import tilesets, instances
 from higlass_manage.start import start
@@ -7,37 +8,42 @@ from higlass_manage.stop import stop
 from higlass_manage.shell import shell
 from higlass_manage.view import view
 from higlass_manage.logs import logs
-from higlass_manage import __version__
+from higlass_manage.create import superuser as create_superuser
+from higlass_manage.delete import superuser as delete_superuser
 
-import higlass_manage.create as hmce
-import higlass_manage.delete as hmde
-
-@click.command()
-def version():
-    print(__version__)
 
 @click.group()
 def list():
-    pass
+    """
+    List instances or datasets
+    """
+
 
 list.add_command(tilesets)
 list.add_command(instances)
+
 
 @click.group()
 def create():
     pass
 
-create.add_command(hmce.superuser)
+
+create.add_command(create_superuser)
+
 
 @click.group()
 def delete():
     pass
 
-delete.add_command(hmde.superuser)
 
+delete.add_command(delete_superuser)
+
+
+@click.version_option(__version__, "-V", "--version")
 @click.group()
 def cli():
     pass
+
 
 cli.add_command(create)
 cli.add_command(delete)
@@ -48,4 +54,3 @@ cli.add_command(stop)
 cli.add_command(shell)
 cli.add_command(view)
 cli.add_command(logs)
-cli.add_command(version)
